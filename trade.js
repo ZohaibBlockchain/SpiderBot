22fb3b6210749654491e3f3c4b543cb1fdef65f8
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-04-14 05:59:44 +0400

    fix

--- trade.js
+++ trade.js
@@ -10,7 +10,7 @@ const binance = new Binance().options({
   APISECRET: process.env.API_SECRET,
 });
 export const IterationTime = 1;//one second
-const desireProfitPercentage = 0.05;
+const desireProfitPercentage = 0.1;
 let totalPNL = 0;
 let ProfitableTrades = 0;
 let lostTrades = 0;
