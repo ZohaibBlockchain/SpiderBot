83ec71d0be7ba1ee37c35de9a1d8e0ebeb784486
ZohaibBlockchain <113122909+ZohaibBlockchain@users.noreply.github.com>
2023-04-14 17:02:23 +0400

    time fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 const app = express();
 const port = 443;
-import {_tradeEngine,IterationTime } from "./trade.js";
+import { _tradeEngine, IterationTime } from "./trade.js";
 
 process.on('uncaughtException', function (err) {
   console.log(err);
@@ -12,21 +12,20 @@ process.on('TypeError', function (err) {
   console.log(err);
 });
 
-let DisplayCounter = 0;
+let delay = 0;
 async function botCore() {
- 
-  await _tradeEngine();
-  setTimeout(botCore, IterationTime*400);
+
+  if (delay > 30) {
+    await _tradeEngine();
+  }
+  else {
+    console.log('Init...')
+  }
+  setTimeout(botCore, IterationTime * 400);
+  delay++;
+
 }
 
- botCore();
+botCore();
 
 
-
-//  if (DisplayCounter > 30) {
-//   console.clear();
-//   console.log('V1 SYTHE AI Bot +_+ : ');
-//   console.log('Current Instruments are:');
-//    console.log('SYTHE BOT Health 100 % and total number of trade are: ');
-//   DisplayCounter = 0;
-// }
